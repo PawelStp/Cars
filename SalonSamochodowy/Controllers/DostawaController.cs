3d730b359405c027d5cb4296b64ece215983ce61
pawel pawel <stypulkowski.pawel1@gmail.com>
2017-12-12 15:40:32 +0100

    added dostawa

--- SalonSamochodowy/Controllers/DostawaController.cs
+++ SalonSamochodowy/Controllers/DostawaController.cs
@@ -29,8 +29,8 @@ namespace SalonSamochodowy.Controllers
         // GET: Dostawa/Create
         public ActionResult Create(int id)
         {
-            
-            return View(new Dostawa { Id_zamowienia=id});
+
+            return View(new Dostawa { Id_zamowienia = id });
         }
 
         // POST: Dostawa/Create
@@ -42,12 +42,51 @@ namespace SalonSamochodowy.Controllers
                 // TODO: Add insert logic here
                 using (var dbContext = new DbContext())
                 {
+                    var zamowiewnie = dbContext.Zamowienia.GetById(dostawa.Id_zamowienia);
+                    var ileDoszlo = dostawa.Ilosc_dostarczonych;
+
+
+                    zamowiewnie.Ilosc_dostarczonych += dostawa.Ilosc_dostarczonych;
+                    var samochod = dbContext.Samochody_fabryczne.GetAll().Where(s => s.Id == zamowiewnie.Id_samochodu_fabryka).FirstOrDefault();
+
+                    if (zamowiewnie.Ilosc_dostarczonych > zamowiewnie.Ilosc_zamowionych)
+                    {
+                        zamowiewnie.Obecny_status = "Zrealizowane";
+                        var zamowione = zamowiewnie.Ilosc_zamowionych;
+                        var dostarczone = zamowiewnie.Ilosc_dostarczonych;
+                        ileDoszlo = zamowione - (dostarczone - ileDoszlo) ?? 0;
+                        zamowiewnie.Ilosc_dostarczonych = zamowiewnie.Ilosc_zamowionych;
+                    }
+                    else if(zamowiewnie.Ilosc_dostarczonych == zamowiewnie.Ilosc_zamowionych)
+                    {
+                        zamowiewnie.Obecny_status = "Zrealizowane";
+                    }
+
+                    dbContext.Zamowienia.Update(zamowiewnie);
                     dbContext.Dostawy.Add(dostawa);
+
+                    for (int i = 0; i < ileDoszlo; i++)
+                    {
+                        Samochod s = new Samochod
+                        {
+                            Cena = samochod.Cena_fabryka+3000,
+                            Data_produkcji = samochod.Data_produkcji,
+                            Marka = samochod.Marka,
+                            Moc_silnika = samochod.Moc_silnika,
+                            Id_dostawy = dostawa.Id,
+                            Model = samochod.Model,
+                            Pojemnosc_silnika = samochod.Pojemnosc_silnika,
+                            Typ_wyposazenia = samochod.Typ_wyposazenia
+                        };
+
+                        dbContext.Samochody.Add(s);
+                    }
                     dbContext.SaveChanges();
+
                 }
                 return RedirectToAction("Index");
             }
-            catch
+            catch(Exception e)
             {
                 return View();
             }
